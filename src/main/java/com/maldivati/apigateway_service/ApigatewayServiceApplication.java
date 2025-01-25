1bd4cf21f2c9fefbf528398ba05833d5279447b2
Wilmer Torres <wilmer.torres@linktic.com>
2025-01-25 12:21:59 -0500

    test jenkins

--- src/main/java/com/maldivati/apigateway_service/ApigatewayServiceApplication.java
+++ src/main/java/com/maldivati/apigateway_service/ApigatewayServiceApplication.java
@@ -2,12 +2,18 @@ package com.maldivati.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(ApigatewayServiceApplication.class);
+
 
 	public static void main(String[] args) {
+		logger.info("Iniciando ApigatewayServiceApplication...");
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
+		logger.info("ApigatewayServiceApplication arrancada exitosamente.");
 	}
 
 }
